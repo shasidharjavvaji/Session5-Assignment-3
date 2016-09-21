bdd696a98a82ae0a793f363908adc495cda04f85
shasidharjavvaji <javvajishasidhar@gmail.com>
2016-09-21 14:16:03 +0530

    Session 5 Assignment 3

---
+++ app.js
@@ -0,0 +1,13 @@
+var employee = {
+        name:"Shasidhar",
+		age:22,
+		Salary:40000,
+		Address : {
+             city:"Vijayawada",
+             state:"Andhra pradesh",
+             pincode:520010			 
+		}
+
+};
+console.log("Name:"+employee.name+"\nAge:"+employee.age+"\nSalary:"+employee.Salary+"\n");
+console.log("City:"+employee.Address.city+"\nState:"+employee.Address.state+"\npincode:"+employee.Address.pincode);
